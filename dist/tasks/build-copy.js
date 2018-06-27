f76bb2ecb9873c70db2b0ce69d87b2931d8d3794
Aleksandr Tsertkov <tsertkov@gmail.com>
2018-06-27 17:10:36 +0200

    Remove gulp-debug and gulp-if modules and their usage from build-copy

--- dist/tasks/build-copy.js
+++ dist/tasks/build-copy.js
@@ -2,14 +2,8 @@
 
 var _gulp = _interopRequireDefault(require("gulp"));
 
-var _gulpDebug = _interopRequireDefault(require("gulp-debug"));
-
-var _gulpIf = _interopRequireDefault(require("gulp-if"));
-
 var _config = _interopRequireDefault(require("../config"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_gulp.default.task('build-copy', () => _gulp.default.src(_config.default.copy.src, {}).pipe((0, _gulpIf.default)(_config.default.isGulpDebug, (0, _gulpDebug.default)({
-  title: 'Files copied:'
-}))).pipe(_gulp.default.dest(_config.default.paths.dst)));
+_gulp.default.task('build-copy', () => _gulp.default.src(_config.default.copy.src, {}).pipe(_gulp.default.dest(_config.default.paths.dst)));
--- src/tasks/build-copy.js
+++ src/tasks/build-copy.js
@@ -1,11 +1,8 @@
 import gulp from 'gulp'
-import debug from 'gulp-debug'
-import gulpif from 'gulp-if'
 import config from '../config'
 
 gulp.task('build-copy', () => (
   gulp
     .src(config.copy.src, {})
-    .pipe(gulpif(config.isGulpDebug, debug({ title: 'Files copied:' })))
     .pipe(gulp.dest(config.paths.dst))
 ))
