7508c3fcfd632b8d00ca9e11eb6779ce9dfee6c3
Aleksandr Tsertkov <tsertkov@gmail.com>
2018-07-24 16:36:32 +0200

    Expect missing tasks dir

--- dist/lib/load-tasks.js
+++ dist/lib/load-tasks.js
@@ -9,12 +9,17 @@ var _path = _interopRequireDefault(require("path"));
 
 var _gulp = _interopRequireDefault(require("gulp"));
 
+var _fs = require("fs");
+
 var _requireDir = _interopRequireDefault(require("require-dir"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function loadTasks(src, configObj) {
-  const tasks = (0, _requireDir.default)(_path.default.join(src, 'tasks'));
+  const tasksPath = _path.default.join(src, 'tasks');
+
+  if (!(0, _fs.existsSync)(tasksPath)) return;
+  const tasks = (0, _requireDir.default)(tasksPath);
   Object.keys(tasks).forEach(task => _gulp.default.task(task, async () => {
     tasks[task](configObj);
   }));
--- src/lib/load-tasks.js
+++ src/lib/load-tasks.js
@@ -1,9 +1,12 @@
 import path from 'path'
 import gulp from 'gulp'
+import { existsSync } from 'fs'
 import requireDir from 'require-dir'
 
 export default function loadTasks (src, configObj) {
-  const tasks = requireDir(path.join(src, 'tasks'))
+  const tasksPath = path.join(src, 'tasks')
+  if (!existsSync(tasksPath)) return
+  const tasks = requireDir(tasksPath)
   Object.keys(tasks).forEach(task => gulp.task(task, async () => {
     tasks[task](configObj)
   }))
